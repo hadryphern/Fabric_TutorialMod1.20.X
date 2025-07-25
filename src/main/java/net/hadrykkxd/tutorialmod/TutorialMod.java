434da4aac9061e2a8da2ce611a075dc83fd27879
hadry <happiesblusk@gmail.com>
2025-07-25 06:09:51 +0100

    Important Additions

--- src/main/java/net/hadrykkxd/tutorialmod/TutorialMod.java
+++ src/main/java/net/hadrykkxd/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.hadrykkxd.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.hadrykkxd.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
